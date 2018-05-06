fcf569c01009369cf71e8725c9400bfd56dbe98c
niclin <bboyceo@hotmail.com>
2018-05-06 22:59:28 +0800

    create & delete

--- app/javascript/todolists.vue
+++ app/javascript/todolists.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h1>My tasks</h1>
+    <h1>Todo Lists</h1>
+
+    <input type="text" v-model="todo" class="form-control" autofocus="true">
+    <button @click="addTodo()" class="btn btn-primary" :disabled="!todo.length">Add Todo</button>
+
     <table>
       <thead>
         <tr>
           <th>#</th>
           <th>Item</th>
+           <th>operate</th>
         </tr>
       </thead>
 
@@ -13,6 +18,9 @@
         <tr v-for="todo in list" >
           <td>{{ todo.id }}</td>
           <td>{{ todo.item }}</td>
+          <td>
+            <button @click="deleteTodo(todo.id)" class="btn btn-primary">Delete Todo</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -23,6 +31,7 @@
 export default {
   data: function () {
     return {
+      todo: '',
       list: [
         { id: 1, item: "Foo" },
         { id: 2, item: "Bar" }
@@ -35,6 +44,18 @@ export default {
   },
 
   methods: {
+    addTodo(){
+      this.$http.post('todolists.json', { item: this.todo }, {})
+      .then((res) => this.fetchTodoLists(), this.todo = '')
+      .catch((error) => console.log('Got a problem' + error));
+    },
+
+    deleteTodo(todo_id){
+      this.$http.delete('todolists/'+ todo_id +'.json')
+      .then((res) => this.fetchTodoLists())
+      .catch((error) => console.log('Got a problem' + error));
+    },
+
     fetchTodoLists: function() {
        const resource = this.$resource('/todolists.json/{ id }');
        resource.get().then(function(response){
